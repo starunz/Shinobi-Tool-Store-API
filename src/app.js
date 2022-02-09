2bc8322bbffae9003136f32b847702d28de37759
starunz <busouza01@gmail.com>
2022-02-09 01:16:35 -0500

    initial structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/index.js';
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use(router);
+
+server.listen(5000, () => {
+    console.log('rodando')
+});
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+//importar as rotas aqui
+const router = Router();
+
+export default router;
