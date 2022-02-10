c659b6d99f5a999f80b4098b56a7cc6a1206a4d6
starunz <busouza01@gmail.com>
2022-02-09 23:09:31 -0500

    implemented login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
-import { signUp } from '../controllers/authController.js';
+import { signUp, signIn } from '../controllers/authController.js';
+
 import { validateSignUp } from '../middlewares/validateSignUp.js';
+import { validateSignIn } from '../middlewares/validateSignIn.js';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', validateSignUp, signUp);
+authRouter.post('/login', validateSignIn, signIn);
 
 export default authRouter;
