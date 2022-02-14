78d39d75aa4455cbe2b7f92a57909d4682f03c14
starunz <busouza01@gmail.com>
2022-02-14 14:06:31 -0500

    deploy

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const server = express();
 
 server.use(express.json());
